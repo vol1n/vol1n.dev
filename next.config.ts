2b931b8e6f6997698095b7a2185dced74eb8e06d
Colin Ryan <colin.ryan.ga+git@gmail.com>
2025-03-19 10:28:45 -0400

    proxy /clojure-workout-tracker to cloudfront

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 import { withContentlayer } from 'next-contentlayer2';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/clojure-workout-tracker/:path*",
+        destination: "https://dimmin3f9flnh.cloudfront.net/clojure-workout-tracker/:path*",
+      },
+    ];
+  },
+};
 
 export default withContentlayer(nextConfig);
