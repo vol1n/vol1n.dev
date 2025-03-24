a03c2448d0b27154c0e4c8289ca22b60fa9e8c4e
Colin Ryan <colin.ryan.ga+git@gmail.com>
2025-03-24 18:06:41 -0400

    add demo crm project, fix workout tracker link :/

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,17 @@
 import type { NextConfig } from "next";
-import { withContentlayer } from 'next-contentlayer2';
+import { withContentlayer } from "next-contentlayer2";
 
 const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
         source: "/clojure-workout-tracker/:path*",
-        destination: "https://dimmin3f9flnh.cloudfront.net/clojure-workout-tracker/:path*",
+        destination:
+          "https://dimmin3f9flnh.cloudfront.net/clojure-workout-tracker/:path*",
+      },
+      {
+        source: "/demo-crm/:path*",
+        destination: "https://demo-crm-chi.vercel.app/:path*",
       },
     ];
   },
